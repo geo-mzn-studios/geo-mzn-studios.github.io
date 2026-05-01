77c3766ad04da166b294037b9344176c69c22da8
geo-studios-official <geo.studios.official@gmail.com>
2026-05-01 20:07:19 +0800

    Fix main.js

--- geoverify/code/main.js
+++ geoverify/code/main.js
@@ -1,6 +1,6 @@
 function copyCode() {
   const text = document.getElementById("code").innerText;
   navigator.clipboard.writeText(text);
-  console.log(text)
+  console.log(text);
   alert("Copied!");
 }
